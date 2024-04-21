a006ca4f49e54319f7de783bafc078a514978f8e
GiuseppePisante <gpisante01@gmail.com>
2024-04-21 10:47:09 +0000

    pivot moved to the back of input vector

--- challenge2.cpp
+++ challenge2.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> partition_vector(std::vector<int> input, int rank, int pivot)
+std::vector<int> partition_vector(std::vector<int> input, int &rank, int pivot)
 {
     std::vector<int> partitioned;
     int k = 0;
+    int tmp = 0;
 
     for (int j = 0; j < input.size(); j++)
     {
-        if (input[j] <= pivot)
+        if (input[j] < pivot)
         {
             if (j == k)
             {
@@ -23,11 +24,27 @@ std::vector<int> partition_vector(std::vector<int> input, int rank, int pivot)
                 k++;
             }
         }
+        else if (input[j] == pivot)
+        {
+            input[j] = input[input.size() - tmp - 1];
+            input[input.size() - tmp - 1] = pivot;
+            tmp++;
+            j--;
+        }
     }
+    for (int j = 0; j < tmp; j++)
+    {
+        int key = input[j + k];
+        input[j + k] = input[input.size() - j - 1];
+        input[input.size() - j - 1] = key;
+    }
+    k += tmp;
+
     if (k < rank)
     {
         std::vector<int>::const_iterator start = input.begin() + k + 1;
         std::vector<int>::const_iterator stop = input.end();
+        rank -= k;
 
         std::vector<int> partitioned(start, stop);
         return partitioned;
@@ -35,7 +52,7 @@ std::vector<int> partition_vector(std::vector<int> input, int rank, int pivot)
     else if (k > rank)
     {
         std::vector<int>::const_iterator start = input.begin();
-        std::vector<int>::const_iterator stop = input.begin() + k;
+        std::vector<int>::const_iterator stop = input.begin() + k - tmp;
 
         std::vector<int> partitioned(start, stop);
         return partitioned;
@@ -99,13 +116,49 @@ int linearTimeOrderStatistics(std::vector<int> input, int rank)
         value = input.at(0);
     return value;
 }
+std::ostream &operator<<(std::ostream &os, const std::vector<int> vec)
+{
+    os << "[ ";
+    for (const auto &val : vec)
+        os << val << " ";
+    os << "]";
+
+    return os;
+}
 
 int main()
 {
-    std::vector<int> input = {97, 2, 4, 55, 60, 70};
+    std::vector<int> input = {97, 34, 16, 13, 25, 57};
+    int rank = 5;
 
-    int value = linearTimeOrderStatistics(input, 2);
-    std::cout << "Valore " << value << std::endl;
+    int pivot = find_pivot(input);
+    input = partition_vector(input, rank, pivot);
+
+    std::cout << pivot << std::endl;
+    std::cout << input << std::endl;
+
+    pivot = find_pivot(input);
+    std::cout << pivot << std::endl;
+    input = partition_vector(input, rank, pivot);
+
+    std::cout << input << std::endl;
+
+    pivot = find_pivot(input);
+    std::cout << pivot << std::endl;
+    input = partition_vector(input, rank, pivot);
+
+    std::cout << input << std::endl;
+
+    pivot = find_pivot(input);
+    std::cout << pivot << std::endl;
+    input = partition_vector(input, rank, pivot);
+
+    std::cout << input << std::endl;
+
+    /*
+        int value = linearTimeOrderStatistics(input, 2);
+        std::cout << "Valore " << value << std::endl;
+        */
 
     return 0;
 }
